5eb927718dfb77a599c13d23e855aea1583f2bd2
pardeepbhasin <pardeep.bhasin@airtel.com>
2017-08-15 01:27:24 +0530

    updated code for save new user

--- react-demo/src/modules/authorization/AuthUserDetails.js
+++ react-demo/src/modules/authorization/AuthUserDetails.js
@@ -6,7 +6,12 @@ import * as authorizeUserAction from '../../common/actions/userActions';
 class AuthUserDetailComponent extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            user : ''
+        }
         this._logout =  this._logout.bind(this);
+        this._addUser = this._addUser.bind(this);
+        this._changeUser = this._changeUser.bind(this);
     }
 
     componentWillMount() {
@@ -15,6 +20,14 @@ class AuthUserDetailComponent extends React.Component {
     render() {
         return (
             <div>
+                <input
+                        className='form__field-input'
+                        type='text'
+                        required="required" 
+                        value={this.state.user}
+                        onChange={this._changeUser}
+                        placeholder='Add User'/>
+                        <button onClick= {this._addUser} className='form__submit-btn' type='submit'>Add User</button>
                 {this.props.users.length ? this.props.users.map(function(item, index) {
                     return <div key={index}>{item.name}</div>;
                 }) : null}
@@ -26,6 +39,14 @@ class AuthUserDetailComponent extends React.Component {
     _logout(event) {
         localStorage.removeItem("appAccessToken");
         this.context.router.push('/login');
+    }
+
+    _changeUser(event) {
+        this.setState({user: event.target.value});
+    }
+
+    _addUser(event) {
+
     }
 }
 
